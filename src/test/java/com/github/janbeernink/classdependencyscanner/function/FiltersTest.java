ef93c9620bd2d47cf4f2306075806a52fa1f8bcd
Jan Beernink <jan.beernink@gmail.com>
2014-04-26 20:30:50 +0200

    Add a number of tests for filter combinations

--- src/test/java/com/github/janbeernink/classdependencyscanner/function/FiltersTest.java
+++ src/test/java/com/github/janbeernink/classdependencyscanner/function/FiltersTest.java
@@ -10,6 +10,22 @@ import org.junit.Test;
 
 public class FiltersTest {
 
+	private static final Filter ALWAYS_TRUE = new Filter() {
+
+		@Override
+		public boolean includeClassInResults(Class<?> clazz) {
+			return true;
+		}
+	};
+
+	private static final Filter ALWAYS_FALSE = new Filter() {
+
+		@Override
+		public boolean includeClassInResults(Class<?> clazz) {
+			return false;
+		}
+	};
+
 	@Test
 	public void testLimitToPackage() {
 		Filter filter = Filters.limitToPackage("java.util");
@@ -18,5 +34,25 @@ public class FiltersTest {
 		assertFalse(filter.includeClassInResults(java.awt.List.class));
 		assertTrue(filter.includeClassInResults(Logger.class));
 		assertFalse(filter.includeClassInResults(String.class));
+	}
+
+	@Test
+	public void testNot() {
+		assertTrue(Filters.not(ALWAYS_FALSE).includeClassInResults(Object.class));
+		assertFalse(Filters.not(ALWAYS_TRUE).includeClassInResults(Object.class));
+	}
+
+	@Test
+	public void testOr() {
+		assertTrue(Filters.or(ALWAYS_TRUE, ALWAYS_FALSE).includeClassInResults(Object.class));
+		assertTrue(Filters.or(ALWAYS_FALSE, ALWAYS_TRUE).includeClassInResults(Object.class));
+		assertFalse(Filters.or(ALWAYS_FALSE, ALWAYS_FALSE).includeClassInResults(Object.class));
+	}
+
+	@Test
+	public void testAnd() {
+		assertTrue(Filters.and(ALWAYS_TRUE, ALWAYS_TRUE).includeClassInResults(Object.class));
+		assertFalse(Filters.and(ALWAYS_FALSE, ALWAYS_TRUE).includeClassInResults(Object.class));
+		assertFalse(Filters.and(ALWAYS_TRUE, ALWAYS_FALSE).includeClassInResults(Object.class));
 	}
 }
