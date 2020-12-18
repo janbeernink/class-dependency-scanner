f5a3ce48bb97764ba2bb6fd138a323a73f0cbd7b
Jan Beernink <jan.beernink@gmail.com>
2020-12-18 12:18:16 +0100

    Fix documentation

--- src/examples/java/com/github/janbeernink/classdependencyscanner/example/StringGraph.java
+++ src/examples/java/com/github/janbeernink/classdependencyscanner/example/StringGraph.java
@@ -21,14 +21,10 @@ import com.github.janbeernink.classdependencyscanner.DependencyGraphNode;
 public class StringGraph {
 
 	public static void main(String[] args) {
-		// tag::graphCreation[]
 		DependencyGraphNode startingNode = new ClassDependencyScanner().buildDependencyGraph(String.class);
-		// end::graphCreation[]
 
-		// tag::iterate[]
 		for (DependencyGraphNode node: startingNode) {
 			// Do something useful
 		}
-		// end::iterate[]
 	}
 }
